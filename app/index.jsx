21aae55668efb27dd810a28fbf462efe98ffb4df
Tanishq <koul.tanishq111@gmail.com>
2024-10-07 17:41:33 +0530

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import {StatusBar } from 'expo-status-bar';
+import React from 'react'
+import {View, Text } from 'react-native';
+import {Link} from "expo-router";
+
+
+export default function App(){
+  return(
+    <View className="flex-1  justify-center items-center bg-white">
+    <Text className="text-3xl font-pextrabold">Aora!</Text>
+    <StatusBar style="auto"/>
+    <Link href="/home" style={{color:"blue"}}>Go to home</Link>
+ 
+  )
+}
+
